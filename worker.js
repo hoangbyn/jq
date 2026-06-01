dadcf07adf7db0f529b998ddef0a9318194ef00a
hoangbyn <89197525+hoangbyn@users.noreply.github.com>
2026-06-01 15:33:37 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,14 +1,11 @@
 export default {
   async fetch() {
-    const callback =
-      "jQuery3510" +
-      Math.floor(Math.random() * 999999999999999999) +
-      "_" +
-      Date.now();
+    const callback = "jQuery3510" + Math.floor(Math.random() * 999999999999999999) + "_" + Date.now();
 
     return new Response(callback, {
       headers: {
-        "Content-Type": "text/plain"
+        "Content-Type": "text/plain",
+        "Cache-Control": "no-store"
       }
     });
   }
