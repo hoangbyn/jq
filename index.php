7bfa10563c9adbe039db21fe37697705751e2a10
hoangbyn <89197525+hoangbyn@users.noreply.github.com>
2026-06-01 15:17:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,24 @@
 export default {
-  async fetch(request, env, ctx) {
+  async fetch() {
+
     const callback =
       "jQuery3510" +
       Math.floor(Math.random() * 999999999999999999) +
       "_" +
       Date.now();
 
-    const url =
-      "https://example.com/test?callback=" +
-      encodeURIComponent(callback);
-
-    const html = `<!DOCTYPE html>
+    return new Response(`
+<!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>JSONP URL Generator</title>
-</head>
 <body>
-
-<h3>Callback:</h3>
 <pre>${callback}</pre>
-
-<h3>Full URL:</h3>
-<pre>${url}</pre>
-
 </body>
-</html>`;
-
-    return new Response(html, {
+</html>
+`, {
       headers: {
-        "Content-Type": "text/html; charset=utf-8",
+        "Content-Type": "text/html;charset=UTF-8",
         "Cache-Control": "no-store"
       }
     });
   }
-};
+}
