262bd3c52caa1af8040e425e50b7cdba4dedb04b
hoangbyn <89197525+hoangbyn@users.noreply.github.com>
2026-06-01 15:15:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,37 @@
-<?php
-$callback = "jQuery3510" .
-    rand(100000000, 999999999) .
-    rand(100000000, 999999999) .
-    "_" .
-    round(microtime(true) * 1000);
+export default {
+  async fetch(request, env, ctx) {
+    const callback =
+      "jQuery3510" +
+      Math.floor(Math.random() * 999999999999999999) +
+      "_" +
+      Date.now();
 
-$url = "https://example.com/test?callback=" . $callback;
-?>
-<!DOCTYPE html>
+    const url =
+      "https://example.com/test?callback=" +
+      encodeURIComponent(callback);
+
+    const html = `<!DOCTYPE html>
 <html>
+<head>
+<meta charset="utf-8">
+<title>JSONP URL Generator</title>
+</head>
 <body>
 
 <h3>Callback:</h3>
-<pre><?php echo $callback; ?></pre>
+<pre>${callback}</pre>
 
 <h3>Full URL:</h3>
-<pre><?php echo $url; ?></pre>
+<pre>${url}</pre>
 
 </body>
-</html>
+</html>`;
+
+    return new Response(html, {
+      headers: {
+        "Content-Type": "text/html; charset=utf-8",
+        "Cache-Control": "no-store"
+      }
+    });
+  }
+};
